116f28f219c2362ec8279b00ab12870a8d75cbd1
sype <sype@sype-beezzonline.(none)>
2013-07-17 16:43:49 +0200

    add function check stat

--- sysadmin/cluster-check/cluster-check2.py
+++ sysadmin/cluster-check/cluster-check2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import MySQLdb
 
-dict={}
+
 db = MySQLdb.connect(host="10.7.20.3", # your host, usually localhost
                      user="spincemail", # your username
                       passwd="Seb4sB9oXx", # your password
@@ -9,15 +9,41 @@ db = MySQLdb.connect(host="10.7.20.3", # your host, usually localhost
 
 # you must create a Cursor object. It will let
 #  you execute all the query you need
-cur = db.cursor() 
+
+def monitor_cluster() :
+	dict={}
+	cur = db.cursor()
+
 
 # Use all the SQL you like
-cur.execute("SHOW status LIKE 'wsrep%';")
+	cur.execute("SHOW status LIKE 'wsrep%';")
 
 
-for row in cur.fetchall() :
-	dict.setdefault(row[0],[]).append(row[1])
-if dict['wsrep_local_state_comment'] == ['Synced']:
-	print "cluster synchronised"
-if dict['wsrep_cluster_state_uuid'] == dict['wsrep_local_state_uuid']:
-	print " Right state"
+	for row in cur.fetchall() :
+		dict.setdefault(row[0],[]).append(row[1])
+	if dict['wsrep_local_state_comment'] == ['Synced']:
+		print "cluster synchronised"
+	if dict['wsrep_cluster_size'] == ['2']:
+		print " All node are up"
+		
+	if dict['wsrep_ready'] == ['ON']:
+		print "Node ready for connection"
+		
+	
+	
+	
+	
+def check_stats() :
+	dict={}
+	cur = db.cursor()
+	cur.execute("SHOW STATUS;")
+	for row in cur.fetchall() :
+		dict.setdefault(row[0],[]).append(row[1])
+	if  dict['Max_used_connections'] > 0:
+		print dict['Max_used_connections'] 
+		
+		
+		
+		
+monitor_cluster()
+check_stats()
