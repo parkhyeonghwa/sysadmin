6b78ed8597ac4839ac06f433b6eefdf46c2ca785
sype <sype@sype-beezzonline.(none)>
2013-08-01 16:28:10 +0200

    ajout du script BI

--- sysadmin/divers-scripts/extract-BI.sh
+++ sysadmin/divers-scripts/extract-BI.sh
@@ -5,12 +5,13 @@ PASSWD=N2O6V6E4M
 REQUETE="SELECT * FROM PRODUCTION.ITEMS_test INTO OUTFILE 'bi-extract.txt' FIELDS TERMINATED BY '|' ENCLOSED BY '' LINES TERMINATED BY '\n';"
 PORT=13815
 HOST=10.10.6.5
-FILE="/var/lib/mysql/sandboxes/multi_msb_5_5_13/node2/data/bi-extract.txt"
+FILES="/var/lib/mysql/sandboxes/multi_msb_5_5_13/node2/data/DATA_PBX_UK.txt","/var/lib/mysql/sandboxes/multi_msb_5_5_13/node2/data/HISTORIC_PBX_UK"
 SUBJECT="BI.txt"
 TO="pincemail.sebastien@gmail.com"
 
 function cleaner_extract {
 
+for FILE in $FILES 
 if [ -f $FILE ];
 
         then rm $FILE
