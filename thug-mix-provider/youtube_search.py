50bc0fba70f8e868ec346f000575e07a5cbd61e4
sype <admin@beezzonline.com>
2013-08-21 17:54:59 +0200

    etat good

---
+++ thug-mix-provider/youtube_search.py
@@ -0,0 +1,35 @@
+__author__ = 'sype'
+
+
+# Import the modules
+import requests
+import json
+
+# Make it a bit prettier..
+print "-" * 30
+print "This will show the Most Popular Videos on YouTube"
+print "-" * 30
+
+# Get the feed
+r = requests.get("http://gdata.youtube.com/feeds/api/videos/?q=mixtape+dancehall&v=2&alt=jsonc")
+r.text
+
+# Convert it to a Python dictionary
+data = json.loads(r.text)
+print data
+
+# Loop through the result.
+for item in data['data']['items']:
+
+    print "Video Title: %s" % (item['title'])
+
+    print "Video Category: %s" % (item['category'])
+
+    print "Video ID: %s" % (item['id'])
+
+    print "Video Rating: %f" % (item['rating'])
+
+    print "Embed URL: %s" % (item['player']['default'])
+    print "Updated : %s" % (item['updated'])
+
+    print
